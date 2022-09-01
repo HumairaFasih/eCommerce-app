256e3e08d77ccf6a7d4cd63969e1f6d707c331fa
Humaira Fasih <humairafasih18@gmail.com>
2022-09-01 19:27:20 +0500

    Revert "Added Routes"
    This reverts commit 5bee885aed165fec1bc9a21d849c716c8ff23f52.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import React from "react"
-import { Routes, Route } from "react-router-dom"
-import Home from "./routes/Home/Home.jsx"
-import Shop from "./routes/Shop/Shop.jsx"
-import NavBar from "./components/navbar/NavBar.jsx"
-import SignIn from "./routes/SignIn/SignIn.jsx"
+import logo from './logo.svg';
+import './App.css';
 
-const App = () => {
+function App() {
   return (
-    <Routes>
-    
-      <Route path="/" element={<NavBar />}>
-        <Route index element={<Home/>} />
-        <Route path="shop" element={<Shop />}/>
-        <Route path="login" element={<SignIn />}/>
-      </Route>
-       
-    </Routes>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
